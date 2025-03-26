9c5fb517d3c248fa057d48a89eec486610dcfc9b
Julian Rios <julianelsensei@gmail.com>
2025-03-25 23:45:00 -0300

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Inmobiliaria_Rios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
